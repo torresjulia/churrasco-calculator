b9d7831f4be06b84fd695649de09ea73c80b60af
Julia Torres <juliadittoro@yahoo.com>
2025-03-31 20:59:58 -0300

    fixing layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
 import { ChurrascoProvider } from "./context/ChurrascoContext";
 import { GlobalStyle } from "./globalStyles";
-import CalculatorForm from "./components/CalculatorForm";
-import Result from "./components/Result";
-import Footer from "./components/Footer";
 import ClarityAnalytics from "./infrastructure/ClarityAnalytics";
 import { CLARITY_PROJECT_ID } from "./config/analytics";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
-    <ThemeProvider theme={{}}>
-      <ChurrascoProvider>
-        <GlobalStyle />
-        <ClarityAnalytics projectId={CLARITY_PROJECT_ID} />
-        <CalculatorForm />
-        <Result />
-        <Footer />
-      </ChurrascoProvider>
-    </ThemeProvider>
+    <ChurrascoProvider>
+      <GlobalStyle />
+      <ClarityAnalytics projectId={CLARITY_PROJECT_ID} />
+      <Calculator />
+    </ChurrascoProvider>
   );
 }
 
